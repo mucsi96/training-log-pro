fe5fcea2a0925a74ca13d54e98215889134bd4de
Igor Bari <mucsi96@gmail.com>
2026-04-30 18:13:42 +0200

    Update Azure Key Vault endpoint in launch configuration and kubeconfig script

--- scripts/pull_kubeconfig.sh
+++ scripts/pull_kubeconfig.sh
@@ -4,6 +4,6 @@ set -e # Exit immediately if a command exits with a non-zero status
 
 mkdir -p .kube
 
-az keyvault secret show --vault-name p06-training-log --name k8s-config --query value --output tsv > .kube/config
+az keyvault secret show --vault-name p07-training-log --name k8s-config --query value --output tsv > .kube/config
 
 chmod 0600 .kube/config
